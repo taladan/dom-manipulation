73b1f24c50a0b5d28fb48d63a139f3779b3a3459
D.J. Crosby <taladan@gmail.com>
2023-02-18 07:37:44 -0500

    Mouse tracking display implemented

--- scripts/mouse_trails.js
+++ scripts/mouse_trails.js
@@ -1,2 +1,7 @@
-title = document.querySelector("h1");
-title.append(" Foo");
+window.addEventListener("mousemove", (event) => {
+  let trailer = document.createElement("div");
+  trailer.className = "trail";
+  trailer.style.left = event.pageX - 12 + "px";
+  trailer.style.top = event.pageY - 12 + "px";
+  document.body.appendChild(trailer);
+});
